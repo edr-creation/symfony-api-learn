36d789be085aa23df50c42799ed65ac95ef1728d
Enzo DO ROSARIO <dorosario.enzo@protonmail.com>
2018-11-24 14:24:05 +0100

    correction d'un bug concernant le DELETE

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Serializer;
 use App\Entity\BlogPost;
